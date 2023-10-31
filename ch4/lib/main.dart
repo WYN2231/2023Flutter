c55b8871c4db9aace3994fa85150c1f341446e59
WYN2231 <147032741+WYN2231@users.noreply.github.com>
2023-10-31 19:56:55 +0900

    Update main.dart

--- ch4/lib/main.dart
+++ ch4/lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'ContainerTest.dart';
 import 'StackTest.dart';
 import 'PuzzleUI.dart';
+import 'PageViewUI.dart';
+import 'TabBarTest.dart';
+import 'BottomNavigationTest.dart';
+import 'CenterTest.dart';
+import 'ButtonTest.dart';
+import 'Test.dart';
+import 'Test2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +28,28 @@ class MyApp extends StatelessWidget {
       ),
       //home: const ContainerTest(),
       //home: const StackTest(),
-      home: const PuzzleUI(),
+      //home: const PuzzleUI(),
+      //home: const PageViewUI(),
+      //home: const TabBarTest(),
+      //home: const BottomNavigationTest(),
+      //home: const CenterTest(),
+      home: const ButtonTest(),
+      //home: Test(),
+      //home: const Test2(),
     );
   }
 }
+
+//Widget build(BuildContext context) {
+//     MaterialApp m = new MaterialApp(  //(원래는 이런 형태, 다트 문법은 위와 같이 표현할 수 있다.) MaterialApp m이라는 생성자를 만듬
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       //home: const ContainerTest(),
+//     );
+
+//     return m; //m을 리턴
+//  }
+//}
