ff317dbf0c4c6c86b814db69b1c07ea75855d3e1
WYN2231 <147032741+WYN2231@users.noreply.github.com>
2023-11-27 20:46:53 +0900

    수업

--- ch7/lib/Page1.dart
+++ ch7/lib/Page1.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Page1 extends StatelessWidget {
   const Page1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
         _buildTop(),
         _buildMiddle(),
@@ -116,10 +117,48 @@ class Page1 extends StatelessWidget {
   }
 
   Widget _buildMiddle() {
-    return const Text('Middle');
+    final imageItems = [
+      'assets/sea.jpg',
+      'assets/dog.jpg',
+      'assets/dinosaur.jpg',
+    ];
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 500.0,
+      ),
+      items: imageItems.map((path)
+          {
+           return Builder(
+               builder: (BuildContext context) {
+                 return Container(
+                   width: MediaQuery.of(context).size.width,
+                   margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                   child: ClipRect(
+                     child: Image.asset(
+                         path,
+                        fit: BoxFit.cover,
+                     ),
+                   ),
+                 );
+               },
+           );
+          }
+      ).toList(),
+    );
   }
 
   Widget _buildBottom() {
-    return const Text('Bottom');
+    final items = List.generate(
+        10, (i){
+          return const ListTile(
+            leading: Icon(Icons.notifications_none),
+            title: Text("공지사항입니다."),
+          );
+    });
+    return ListView(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      children: items,
+    );
   }
 }
